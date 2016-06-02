bb88e90b4fde6059116cdac71997959486cd88d7
FreckledKev <kevin.hulsing@plus3it.com>
2016-06-02 13:52:13 -0400

    Moved bootstrap files to static/shell/bootstraps. Windows PowerShell bootstrap installs Python, upgrades pip. Resets environment variable when Python added to PATH.

---
+++ watchmaker/static/shell/bootstrappers/windows-image-bootstrap.ps1
@@ -0,0 +1,80 @@
+<powershell>
+# Location to save files.
+$SaveDir = ${env:Temp}
+
+# Log file for debugging user-data.
+$LogFile = "${SaveDir}\UserData_Output_$(Get-Date -Format `"yyyyMMdd_hhmmsstt`").log"
+Start-Transcript -Path "${SaveDir}\Transcript_Output_$(Get-Date -Format `"yyyyMMdd_hhmmsstt`").log" -Append
+
+# Urls for obtaining Python and associated tools.
+$Version = "2.7.11"
+$PythonMSI = 'python-' + ${Version} + '.msi';
+$PythonUrl = 'https://www.python.org/ftp/python/' + ${Version} + '/' + ${PythonMSI}
+
+function Log {
+  Param( [string]$Message )
+  $Message | Out-File -Filepath $LogFile -Append
+}
+
+function Install-Python-MSI {
+  Param( [string]$PathToMSI )
+  $Arguments = @()
+  $Arguments += "/i"
+  $Arguments += "`"${PathToMSI}`""
+  $Arguments += "ALLUSERS=`"1`""
+  $Arguments += "/passive"
+  Start-Process "msiexec.exe" -ArgumentList ${Arguments} -Wait
+}
+
+function Download-File {
+  Param( [string]$Url, [string]$SavePath )
+  # Download a file, if it doesn't already exist.
+  if( !(Test-Path ${SavePath} -PathType Leaf) ) {
+    (New-Object System.Net.WebClient).DownloadFile(${Url}, ${SavePath})
+    Log "Downloaded ${Url} to ${SavePath}."
+  }
+}
+
+function Reset_Env_Variable {
+  foreach( $Level in "Machine", "User" ) {
+    [Environment]::GetEnvironmentVariables($Level).GetEnumerator() | % {
+      # For Path variables, append the new values, if they're not already in there.
+      if($_.Name -match 'Path$') { 
+        $_.Value = ($((Get-Content "Env:$($_.Name)") + ";$($_.Value)") -split ';' | Select -unique) -join ';'
+      }
+      $_
+    } | Set-Content -Path { "Env:$($_.Name)" }
+  }
+}
+
+function Get-Python {
+  $SavePath = '' + $SaveDir + '\' + $PythonMSI;
+  if( !(Test-Path -PathType Container $SaveDir) ) {
+    Log "${SaveDir} does not exist."
+    exit
+  }
+
+  Download-File $PythonUrl $SavePath
+  Install-Python-MSI ${SavePath}
+  Log "Installed Python ${Version}."
+
+  [Environment]::SetEnvironmentVariable("Path", "${env:Path};C:\Python27\;C:\Python27\Scripts\", "Machine")
+  Log "Added Python directories to environment variable, PATH."
+  Reset_Env_Variable
+
+  # Upgrade pip.
+  # pip install --upgrade pip : Do not use this as it throws an access denied error.
+  easy_install -U pip
+  Log "Upgraded pip using easy_intall."
+  
+  # Install Python dependencies for WatchMaker -- actually, WatcMaker will take care of these.
+  #pip install boto3
+  #pip install pyyaml
+  #Log "Added Python modules, boto3 and pyyaml, for WatchMaker."
+}
+
+Get-Python
+Log "UserData PowerShell script finished."
+Stop-Transcript
+</powershell>
+<persist>false</persist>
