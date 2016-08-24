10eff2caf70dc6e9ef44c3fa42419cf03abbe0ff
Loren Gordon <loren@fleet-it.com>
2016-08-24 16:08:17 -0400

    Deploys dev builds to testpypi and tagged releases to pypi
    *   Updates .travis.yml with the configuration data to deploy to
        both pypi and testpypi
    *   Adds the script, `travis_set_build.py`, which will update the
        __version__ string with the Travis build number
    
    The idea is to use the `skip` param of the new script in conjunction
    with the $TRAVIS_TAG variable to control whether to update the
    __version__ string with the Travis build number. If the build is
    not a tag, it should be deployed to testpypi, with a build version.
    If the build is a tag, it should be deployed to production pypi and
    the version should be updated outside of Travis.

---
+++ ci/travis_set_build.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Update the version string with a build number"""
+
+from __future__ import absolute_import
+from __future__ import print_function
+
+import io
+import os
+import re
+import sys
+
+
+PROJECT_ROOT = os.path.abspath(os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    os.pardir)
+)
+BUILD_NUMBER = os.environ.get('TRAVIS_BUILD_NUMBER', '')
+VERSION_FILE_PATHS = ('src', 'watchmaker', '__init__.py')
+
+
+def replace(file_path, pattern, repl, flags=0):
+    with io.open(file_path, mode="r+") as fh_:
+        file_contents = fh_.read()
+        file_contents = re.sub(pattern, repl, file_contents)
+        fh_.seek(0)
+        fh_.truncate()
+        fh_.write(file_contents)
+
+
+def main(args):
+    skip = args.skip
+    build = args.build
+    file_paths = args.file_paths
+
+    if skip:
+        print(
+            'Not updating version for this build, `skip` set to "{0}"'
+            .format(skip)
+        )
+        sys.exit(0)
+
+    # The version line must have the form
+    # __version__ = 'ver'
+    pattern = r"^(__version__ = ['\"])([^'\"]*)(['\"])"
+    repl = r"\g<1>\g<2>.dev{0}\g<3>".format(build)
+    version_file = os.path.join(PROJECT_ROOT, *file_paths)
+    print(
+        'Updating version in version_file "{0}" with build "{1}"'
+        .format(version_file, build)
+    )
+    replace(version_file, pattern, repl, flags=re.M)
+
+
+if '__main__' == __name__:
+    import argparse
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        '--skip',
+        default=False,
+        help=(
+            'If set to any non-false value, skips updating the version. '
+            '(default: {0})'.format(False)
+        )
+    )
+    parser.add_argument(
+        '--build',
+        default=BUILD_NUMBER,
+        help=(
+            'Build number to set. Will default to the env TRAVIS_BUILD_NUMBER '
+            'or an empty string. (default: {0})'.format(BUILD_NUMBER)
+        )
+    )
+    parser.add_argument(
+        '--file-paths',
+        default=VERSION_FILE_PATHS,
+        help=(
+            'Tuple of paths relative to the project root to a file containing '
+            'the version string. (default: {0})'.format(VERSION_FILE_PATHS)
+        )
+    )
+
+    args = parser.parse_args()
+    main(args)
--- setup.py
+++ setup.py
@@ -2,13 +2,37 @@
 # -*- encoding: utf-8 -*-
 from __future__ import absolute_import, print_function
 
+import io
 import os
+import re
 
 from setuptools import find_packages, setup
 
+
+def read(*names, **kwargs):
+    return io.open(
+        os.path.join(os.path.dirname(__file__), *names),
+        encoding=kwargs.get('encoding', 'utf8')
+    ).read()
+
+
+def find_version(*file_paths):
+    # Read the version number from a source file.
+    # Why read it, and not import?
+    # see https://groups.google.com/d/topic/pypa-dev/0PkjVpcxTzQ/discussion
+    version_file = read(*file_paths, encoding='latin1')
+
+    # The version line must have the form
+    # __version__ = 'ver'
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
 setup(
     name='watchmaker',
-    version='0.0.1',
+    version=find_version('src', 'watchmaker', '__init__.py'),
     author='Plus3IT Maintainers of Watchmaker',
     author_email='projects@plus3it.com',
     url='https://github.com/plus3it/watchmaker',
