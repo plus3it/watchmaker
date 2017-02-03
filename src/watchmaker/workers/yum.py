a6aca705d64726cdc6fd7413a758a250cf378522
Loren Gordon <loren@fleet-it.com>
2017-02-03 07:26:27 -0500

    Renames epel_version to el_version
    More accurately represents the intent of the parameter.

--- src/watchmaker/workers/yum.py
+++ src/watchmaker/workers/yum.py
@@ -14,10 +14,10 @@ class Yum(LinuxManager):
         super(Yum, self).__init__()
         self.dist = None
         self.version = None
-        self.epel_version = None
+        self.el_version = None
 
     @staticmethod
-    def _get_amazon_epel_version(version):
+    def _get_amazon_el_version(version):
         # All amzn linux distros currently available use el6-based packages.
         # When/if amzn linux switches a distro to el7, rethink this.
         return '6'
@@ -26,7 +26,7 @@ class Yum(LinuxManager):
         """Validate the Linux distro and set associated attributes."""
         self.dist = None
         self.version = None
-        self.epel_version = None
+        self.el_version = None
 
         supported_dists = ('amazon', 'centos', 'red hat')
 
@@ -63,13 +63,13 @@ class Yum(LinuxManager):
             x.translate(None, ' ') for x in matched.groups()
         )
 
-        # Determine epel_version
+        # Determine el_version
         if 'amazon' == self.dist:
-            self.epel_version = self._get_amazon_epel_version(self.version)
+            self.el_version = self._get_amazon_el_version(self.version)
         else:
-            self.epel_version = self.version.split('.')[0]
+            self.el_version = self.version.split('.')[0]
 
-        if self.epel_version is None:
+        if self.el_version is None:
             msg = (
                 'Unsupported OS version! dist = {0}, version = {1}.'
                 .format(self.dist, self.version)
@@ -79,7 +79,7 @@ class Yum(LinuxManager):
 
         self.log.debug('Dist\t\t{0}'.format(self.dist))
         self.log.debug('Version\t\t{0}'.format(self.version))
-        self.log.debug('EPEL Version\t{0}'.format(self.epel_version))
+        self.log.debug('EPEL Version\t{0}'.format(self.el_version))
 
     def _repo(self, config):
         """Validate the ``yumrepomap`` is properly formed."""
@@ -115,22 +115,21 @@ class Yum(LinuxManager):
 
         # TODO This block is weird.  Correct and done.
         for repo in config['yumrepomap']:
-
             if repo['dist'] in [self.dist, 'all']:
                 self.log.debug(
                     '{0} in {1} or all'.format(repo['dist'], self.dist)
                 )
-                if 'epel_version' in repo and \
-                        str(repo['epel_version']) != str(self.epel_version):
+                if 'el_version' in repo and \
+                        str(repo['el_version']) != str(self.el_version):
                     self.log.debug(
-                        'Skipping repo - epel_version ({0}) is not valid for '
+                        'Skipping repo - el_version ({0}) is not valid for '
                         'this repo ({1}).'
-                        .format(self.epel_version, repo['url'])
+                        .format(self.el_version, repo['url'])
                     )
                 else:
                     self.log.info(
                         'All requirements have been validated for repo - {0}.'
-                        .format(self.epel_version, repo['url'])
+                        .format(self.el_version, repo['url'])
                     )
                     # Download the yum repo definition to /etc/yum.repos.d/
                     url = repo['url']
