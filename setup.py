d4d8f3e908345caac82ab3cae68f4fe0d793d4fe
Loren Gordon <loren@fleet-it.com>
2017-02-20 17:35:04 -0500

    Updates package description and metadata
    Fixes #133

--- setup.py
+++ setup.py
@@ -34,15 +34,51 @@ def find_version(*file_paths):
     raise RuntimeError("Unable to find version string.")
 
 
+def parse_md_to_rst(file):
+    """Read Markdown file and convert to ReStructured Text."""
+    try:
+        from m2r import parse_from_file
+        return(parse_from_file(file))
+    except ImportError:
+        # m2r may not be installed in user environment
+        return(read(file))
+
+
 setup(
     name='watchmaker',
     version=find_version('src', 'watchmaker', '__init__.py'),
+    license='Apache Software License 2.0',
     author='Plus3IT Maintainers of Watchmaker',
     author_email='projects@plus3it.com',
+    description='Applied Configuration Management',
+    long_description=parse_md_to_rst('README.md'),
     url='https://github.com/plus3it/watchmaker',
     packages=find_packages(str('src')),
     package_dir={'': str('src')},
     include_package_data=True,
+    platforms=[
+        'Windows',
+        'Linux'
+    ],
+    classifiers=[
+        # complete classifier list:
+        # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Utilities',
+    ],
     entry_points={
         'console_scripts': [
             'watchmaker = watchmaker.cli:main',
