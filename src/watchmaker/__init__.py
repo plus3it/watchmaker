816cb2380ce57246c36ae1223cc80f50d52b061e
Loren Gordon <lorengordon@users.noreply.github.com>
2017-07-08 08:38:39 -0400

    Prepares 0.5.1 release

--- src/watchmaker/__init__.py
+++ src/watchmaker/__init__.py
@@ -19,7 +19,7 @@ from watchmaker.exceptions import WatchmakerException
 from watchmaker.managers.workers import (LinuxWorkersManager,
                                          WindowsWorkersManager)
 
-__version__ = '0.5.1.dev'
+__version__ = '0.5.1'
 
 
 class Arguments(dict):
