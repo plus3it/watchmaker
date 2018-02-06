fcb12a0a2038368dbb7b53131b6d7b70f6619ad4
Loren Gordon <lorengordon@users.noreply.github.com>
2018-02-06 07:22:26 -0500

    Uses a util function to return the filename from a uri
    Closes #508

--- src/watchmaker/utils/__init__.py
+++ src/watchmaker/utils/__init__.py
@@ -8,22 +8,33 @@ import os
 from watchmaker.utils.urllib import parse, request
 
 
-def path_to_uri(filepath):
-    """Convert a file path to a URI compatible with urllib."""
-    parts = parse.urlparse(filepath)
-
+def scheme_from_uri(uri):
+    """Return a scheme from a parsed uri."""
     # Handle case where path does not contain a scheme
     # i.e. '/abspath/foo' or 'relpath/foo'
     # Do not test `if parts.scheme` because of how urlparse handles Windows
     # file paths -- i.e. 'C:\\foo' => scheme = 'c' :(
-    scheme = parts.scheme if '://' in filepath else 'file'
+    return uri.scheme if '://' in parse.urlunparse(uri) else 'file'
+
+
+def uri_from_filepath(filepath):
+    """Return a URI compatible with urllib, handling URIs and file paths."""
+    parts = parse.urlparse(filepath)
+    scheme = scheme_from_uri(parts)
 
     if scheme != 'file':
         # Return non-file paths unchanged
         return filepath
 
-    # Expand relative file paths and convert them to url-style
+    # Expand relative file paths and convert them to uri-style
     path = request.pathname2url(os.path.abspath(os.path.expanduser(
         ''.join([x for x in [parts.netloc, parts.path] if x]))))
 
     return parse.urlunparse((scheme, '', path, '', '', ''))
+
+
+def basename_from_uri(uri):
+    """Return the basename/filename/leaf part of a URI."""
+    # Do not split on '/' and return the last part because that will also
+    # include any query in the uri. Instead, parse the uri.
+    return os.path.basename(parse.urlparse(uri).path)
