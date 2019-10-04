bbca49d8de92ac48d26466a8def91793b9223444
Dirk Avery <dirk.avery@gmail.com>
2019-10-04 13:48:20 -0400

    Move to native markdown proc for wheel instead of m2r

--- setup.py
+++ setup.py
@@ -4,35 +4,8 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals, with_statement)
 
-import io
-import os
-
 from setuptools import setup
 
-
-def read(*names, **kwargs):
-    """Read a file."""
-    return io.open(
-        os.path.join(os.path.dirname(__file__), *names),
-        encoding=kwargs.get('encoding', 'utf8')
-    ).read()
-
-
-def parse_md_to_rst(files):
-    """Read Markdown files and convert them to ReStructured Text."""
-    rst = []
-    try:
-        from m2r import parse_from_file
-        for name in files:
-            rst += [parse_from_file(name)]
-    except ImportError:
-        # m2r may not be installed in user environment
-        for name in files:
-            rst += [read(name)]
-    return '\n'.join(rst)
-
-
 setup(
-    long_description=parse_md_to_rst(['README.md', 'CHANGELOG.md']),
     package_dir={'': str('src')}
 )
