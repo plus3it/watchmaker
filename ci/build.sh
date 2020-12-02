c231cfd9ec75361689a58347a732b85bedb85112
Loren Gordon <lorengordon@users.noreply.github.com>
2020-12-02 12:37:17 -0800

    Ensures standalone build uses pinned versions of build deps

--- ci/build.sh
+++ ci/build.sh
@@ -2,16 +2,24 @@
 
 export VIRTUAL_ENV_DIR=venv
 
-virtualenv $VIRTUAL_ENV_DIR
+python3 -m ensurepip --upgrade --default-pip
+python3 -m pip install -r requirements/pip.txt
+python3 -m pip --version
+
+python3 -m pip install -r requirements/basics.txt
+python3 -m pip list
+
+virtualenv --python=python3 $VIRTUAL_ENV_DIR
 source $VIRTUAL_ENV_DIR/bin/activate
 
-pip install -r requirements/pip.txt
+python -m pip install -r requirements/pip.txt
 echo "-----------------------------------------------------------------------"
 python --version
 pip --version
 echo "-----------------------------------------------------------------------"
-pip install -r requirements/build.txt
-pip install --editable .
+python -m pip install -r requirements/build.txt
+python -m pip install --editable .
+python -m pip list
 
 # creates standalone
 gravitybee --src-dir src --sha file --with-latest --extra-data static --extra-data ../vendor/pypa/get-pip/2.6 --extra-pkgs boto3 --extra-modules boto3
