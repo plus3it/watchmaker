7c3fb525989bd459a107079b25e8ce8f5e769fe9
Loren Gordon <lorengordon@users.noreply.github.com>
2021-03-23 06:35:20 -0700

    Removes reinstallation of docker-ce
    This was only needed in old travis-ci runtimes

--- ci/prep_docker.sh
+++ ci/prep_docker.sh
@@ -6,30 +6,6 @@ echo "***********************************************************************"
 echo "Prepping for Docker Watchmaker build on $(lsb_release -a)"
 echo "***********************************************************************"
 
-echo "Remove old docker versions..."
-# if unable to remove, it is not a problem
-apt-get -y remove docker docker-engine docker.io || true
-
-echo "Install new docker..."
-# https://docs.docker.com/install/linux/docker-ce/ubuntu/
-apt-get update
-apt-get -y install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-apt-key fingerprint 0EBFCD88
-
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-apt-get update
-apt-get -y install docker-ce docker-ce-cli containerd.io
-
 if [ -n "${DOCKER_SLUG}" ]; then
 
   echo "Installing Docker container..."
