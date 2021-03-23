29405448c337c5ac9bdb287a0fa6bc85090fdc37
Loren Gordon <lorengordon@users.noreply.github.com>
2021-03-23 06:22:28 -0700

    Removes hardcoded use of sudo in standalone build scripts

--- ci/prep_docker.sh
+++ ci/prep_docker.sh
@@ -8,12 +8,12 @@ echo "***********************************************************************"
 
 echo "Remove old docker versions..."
 # if unable to remove, it is not a problem
-sudo apt-get -y remove docker docker-engine docker.io || true
+apt-get -y remove docker docker-engine docker.io || true
 
 echo "Install new docker..."
 # https://docs.docker.com/install/linux/docker-ce/ubuntu/
-sudo apt-get update
-sudo apt-get -y install \
+apt-get update
+apt-get -y install \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -21,19 +21,19 @@ sudo apt-get -y install \
     software-properties-common
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
+apt-key fingerprint 0EBFCD88
 
-sudo add-apt-repository \
+add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-sudo apt-get update
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+apt-get update
+apt-get -y install docker-ce docker-ce-cli containerd.io
 
 if [ -n "${DOCKER_SLUG}" ]; then
 
   echo "Installing Docker container..."
-  sudo docker pull "${DOCKER_SLUG}":latest
+  docker pull "${DOCKER_SLUG}":latest
 
   DOCKER_INSTANCE_NAME="${DOCKER_INSTANCE_NAME:-wam-builder}"
   echo "DOCKER_INSTANCE_NAME:${DOCKER_INSTANCE_NAME}"
@@ -45,17 +45,17 @@ if [ -n "${DOCKER_SLUG}" ]; then
   if [ -n "${WORKDIR}" ]; then
 
     echo "Building using container and workdir (${WORKDIR})..."
-    sudo docker run --detach --privileged \
+    docker run --detach --privileged \
       --volume="${WORKDIR}":"${WORKDIR}" \
       --workdir "${WORKDIR}" \
       --name "${DOCKER_INSTANCE_NAME}" \
       "${DOCKER_SLUG}":latest \
       init
 
-    sudo docker exec "${DOCKER_INSTANCE_NAME}" chmod +x ci/build.sh
-    sudo docker exec "${DOCKER_INSTANCE_NAME}" ci/build.sh
+    docker exec "${DOCKER_INSTANCE_NAME}" chmod +x ci/build.sh
+    docker exec "${DOCKER_INSTANCE_NAME}" ci/build.sh
 
-    sudo docker stop "${DOCKER_INSTANCE_NAME}"
+    docker stop "${DOCKER_INSTANCE_NAME}"
 
   else
 
