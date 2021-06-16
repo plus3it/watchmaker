e0df51a4a192f67dab4c5ea7162ecc81b19ba7ef
Loren Gordon <lorengordon@users.noreply.github.com>
2021-06-16 06:42:13 -0700

    Uses new "public" path and replaces get-pip2.6 with get-pip2.7
    Watchmaker still supports python2.7, so using the lowest common
    denominator for get-pip...

--- ci/build.sh
+++ ci/build.sh
@@ -24,7 +24,7 @@ python -m pip install --editable .
 python -m pip list
 
 # creates standalone
-gravitybee --src-dir src --sha file --with-latest --extra-data static --extra-data ../vendor/pypa/get-pip/2.6 --extra-pkgs boto3 --extra-modules boto3
+gravitybee --src-dir src --sha file --with-latest --extra-data static --extra-data ../vendor/pypa/get-pip/public/2.7 --extra-pkgs boto3 --extra-modules boto3
 
 source .gravitybee/gravitybee-environs.sh
 eval "$GB_ENV_GEN_FILE_W_PATH" --version
