8288ef1fdf17583ba58845d1df4fdf7a832734e0
Loren Gordon <lorengordon@users.noreply.github.com>
2021-09-10 11:04:33 -0700

    Updates test to check for None explicitly

--- tests/test_watchmaker.py
+++ tests/test_watchmaker.py
@@ -64,11 +64,11 @@ def test_none_arguments():
     }
     watchmaker_arguments = watchmaker.Arguments(**dict(**raw_arguments))
 
-    assert not watchmaker_arguments.admin_groups
-    assert not watchmaker_arguments.admin_users
-    assert not watchmaker_arguments.computer_name
-    assert not watchmaker_arguments.salt_states
-    assert not watchmaker_arguments.ou_path
+    assert watchmaker_arguments.admin_groups is None
+    assert watchmaker_arguments.admin_users is None
+    assert watchmaker_arguments.computer_name is None
+    assert watchmaker_arguments.salt_states is None
+    assert watchmaker_arguments.ou_path is None
 
 
 def test_argument_default_value():
