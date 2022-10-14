380070f2ce01ac5f9ea7034a68e88ea77ed76cea
Dennis Carey <dwc0011@users.noreply.github.com>
2022-10-14 10:10:51 -0400

    Remove unneeded file

--- tests/resources/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Loads static module for path operations."""
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals, with_statement)
