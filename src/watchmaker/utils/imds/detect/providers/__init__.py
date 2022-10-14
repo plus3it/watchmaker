2b026c4b436d47888bd9246e70ec3d88557e1d4e
Dennis Carey <dwc0011@users.noreply.github.com>
2022-10-13 21:00:08 -0400

    Add futures and fix typo

--- src/watchmaker/utils/imds/detect/providers/__init__.py
+++ src/watchmaker/utils/imds/detect/providers/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-"""Providrs module."""
+"""Providers module."""
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals, with_statement)
