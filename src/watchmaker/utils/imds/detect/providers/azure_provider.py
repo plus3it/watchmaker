d0cd617a5d1694afd2b6ca97013eb158cf851ac6
Dennis Carey <dwc0011@users.noreply.github.com>
2022-12-21 19:56:01 -0500

    Add logging message for checking vendor file

--- src/watchmaker/utils/imds/detect/providers/azure_provider.py
+++ src/watchmaker/utils/imds/detect/providers/azure_provider.py
@@ -13,24 +13,24 @@ from watchmaker.utils.imds.detect.providers.provider import AbstractProvider
 class AzureProvider(AbstractProvider):
     """Concrete implementation of the Azure cloud provider."""
 
-    identifier = 'azure'
+    identifier = "azure"
 
     def __init__(self, logger=None):
         self.logger = logger or logging.getLogger(__name__)
         self.metadata_url = (
-            'http://169.254.169.254/metadata/instance?api-version=2021-02-01'
+            "http://169.254.169.254/metadata/instance?api-version=2021-02-01"
         )
-        self.vendor_file = '/sys/class/dmi/id/sys_vendor'
-        self.headers = {'Metadata': 'true'}
+        self.vendor_file = "/sys/class/dmi/id/sys_vendor"
+        self.headers = {"Metadata": "true"}
 
     def identify(self):
         """Identify Azure using all the implemented options."""
-        self.logger.info('Try to identify Azure')
+        self.logger.info("Try to identify Azure")
         return self.check_vendor_file() or self.check_metadata_server()
 
     def check_metadata_server(self):
         """Identify Azure via metadata server."""
-        self.logger.debug('Checking Azure metadata')
+        self.logger.debug("Checking Azure metadata")
         try:
             return self.__is_valid_server()
         except BaseException as ex:
@@ -42,18 +42,21 @@ class AzureProvider(AbstractProvider):
 
         Read file /sys/class/dmi/id/sys_vendor
         """
-        self.logger.debug('Checking Azure vendor file')
+        self.logger.debug("Checking Azure vendor file")
 
         # Convert a local vendor file to a URI
         if exists(self.vendor_file):
+            self.logger.debug("Vendor file exists")
             vendor_file_path = utils.uri_from_filepath(self.vendor_file)
             try:
                 check_vendor_file = utils.urlopen_retry(
-                    vendor_file_path, self.DEFAULT_TIMEOUT)
+                    vendor_file_path, self.DEFAULT_TIMEOUT
+                )
                 if "Microsoft Corporation" in check_vendor_file.read():
                     return True
-            except (ValueError, utils.urllib.error.URLError):
-                pass
+            except (ValueError, utils.urllib.error.URLError) as err:
+                self.logger.error("Error while checking vendor file", err)
+
         return False
 
     def __is_valid_server(self):
