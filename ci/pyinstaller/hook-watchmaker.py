6c41c5e03a84668554983dccbc9d7b9dfeea447b
Loren Gordon <lorengordon@users.noreply.github.com>
2023-01-31 10:56:39 -0800

    Avoids collect_all due to differences between pacakge name and module name
    Warnings were of the form:
    
    WARNING: Unable to find package for requirement six from package watchmaker
    WARNING: Unable to find package for requirement PyYAML from package watchmaker

--- ci/pyinstaller/hook-watchmaker.py
+++ ci/pyinstaller/hook-watchmaker.py
@@ -3,7 +3,8 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals, with_statement)
 
-from PyInstaller.utils.hooks import collect_all, copy_metadata
+from PyInstaller.utils.hooks import (collect_data_files, collect_dynamic_libs,
+                                     collect_submodules, copy_metadata)
 
 datas = []
 binaries = []
@@ -11,7 +12,6 @@ hiddenimports = [
     "boto3",
 ]
 datas += copy_metadata("watchmaker", recursive=True)
-tmp_ret = collect_all("watchmaker")
-datas += tmp_ret[0]
-binaries += tmp_ret[1]
-hiddenimports += tmp_ret[2]
+datas += collect_data_files("watchmaker")
+binaries += collect_dynamic_libs("watchmaker")
+hiddenimports += collect_submodules("watchmaker")
