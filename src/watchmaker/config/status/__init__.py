0e48581faef25a47c6b36a36e3be4fe2c873e28b
Dennis Carey <dwc0011@users.noreply.github.com>
2023-02-21 15:32:05 -0500

    Add in method to check for prereqs

--- src/watchmaker/config/status/__init__.py
+++ src/watchmaker/config/status/__init__.py
@@ -3,6 +3,7 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals, with_statement)
 
+
 import logging
 
 from watchmaker.conditions import HAS_AZURE, HAS_BOTO3
@@ -61,7 +62,7 @@ def get_provider_key(provider):
 
 def is_provider_required(provider):
     """Get whether provider required."""
-    return provider["required"]
+    return provider.get("required", "false")
 
 
 def get_provider_type(provider):
@@ -78,7 +79,7 @@ def get_providers_by_provider_types(config_status, provider_type):
     ]
 
 
-def get_supported_cloud_identifiers(config_status):
+def get_supported_cloud_identifiers_with_prereqs(config_status):
     """Get unique list of cloud ids that are supported and we have a config."""
     supported = get_cloud_ids_with_prereqs()
     return list(
@@ -90,12 +91,41 @@ def get_supported_cloud_identifiers(config_status):
     )
 
 
+def get_required_cloud_identifiers_missing_prereqs(config_status):
+    """Get unique list of cloud ids that are supported and we have a config."""
+    supported = get_cloud_ids_without_prereqs()
+    return list(
+        set(
+            provider.get("provider_type").lower()
+            for provider in config_status.get("providers", [])
+            if provider.get("provider_type", "").lower() in supported
+            and provider.get("required", "false")
+        )
+    )
+
+
 def get_cloud_ids_with_prereqs():
     """Get unique list of supported cloud identifiers where prereqs is True."""
     providers = set()
 
     for cloud_provider in SUPPORTED_CLOUD_PROVIDERS:
         if cloud_provider["has_prereq"]:
+            providers.add(cloud_provider["provider"])
+        else:
+            logging.debug(
+                "Skipping provider %s prereqs not found",
+                cloud_provider["provider"],
+            )
+
+    return list(providers)
+
+
+def get_cloud_ids_without_prereqs():
+    """Get unique list of supported cloud identifiers where prereqs is True."""
+    providers = set()
+
+    for cloud_provider in SUPPORTED_CLOUD_PROVIDERS:
+        if not cloud_provider["has_prereq"]:
             providers.add(cloud_provider["provider"])
         else:
             logging.debug(
