c91d180d239784cff18a45504044093bec62f258
Dennis Carey <dwc0011@users.noreply.github.com>
2023-02-21 19:53:43 -0500

    Add in additional tests

--- tests/test_status_config.py
+++ tests/test_status_config.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 """Providers main test module."""
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals, with_statement)
+from __future__ import (
+    absolute_import,
+    division,
+    print_function,
+    unicode_literals,
+    with_statement,
+)
 
 # Supports Python2 and Python3 test mocks
 try:
@@ -11,6 +16,10 @@ except ImportError:
 
 from watchmaker.config.status import (
     get_supported_cloud_identifiers_with_prereqs,
+    get_required_cloud_identifiers_missing_prereqs,
+    get_cloud_ids_with_prereqs,
+    get_cloud_ids_missing_prereqs,
+    get_non_cloud_identifiers,
 )
 
 
@@ -18,8 +27,8 @@ from watchmaker.config.status import (
     "watchmaker.config.status.get_cloud_ids_with_prereqs",
     return_value=["aws", "azure"],
 )
-def test_get_provider_identifiers(prereqs):
-    """Test status config allowed provider_types."""
+def test_get_supported_cloud_identifiers_with_prereqs(prereqs):
+    """Test get required ids that are have prereqs."""
     config_status = {
         "providers": [
             {
@@ -47,3 +56,131 @@ def test_get_provider_identifiers(prereqs):
     assert "azure" in ids
     assert "gcp" not in ids
     assert "none" not in ids
+
+
+@patch(
+    "watchmaker.config.status.get_cloud_ids_missing_prereqs",
+    return_value=["aws", "azure"],
+)
+def test_get_required_cloud_identifiers_missing_prereqs(prereqs):
+    """Test get required ids that are missing prereqs."""
+    config_status = {
+        "providers": [
+            {
+                "key": "WatchmakerStatus",
+                "required": False,
+                "provider_type": "aws",
+            },
+            {
+                "key": "WatchmakerStatus",
+                "required": True,
+                "provider_type": "azure",
+            },
+            {
+                "key": "WatchmakerStatus",
+                "required": False,
+                "provider_type": "gcp",
+            },
+        ]
+    }
+
+    ids = get_required_cloud_identifiers_missing_prereqs(config_status)
+
+    assert ids is not None
+    assert "aws" not in ids
+    assert "azure" in ids
+    assert "gcp" not in ids
+    assert "none" not in ids
+
+
+@patch(
+    "watchmaker.config.status.SUPPORTED_CLOUD_PROVIDERS",
+    [
+        {"provider": "aws", "has_prereq": True},
+        {"provider": "azure", "has_prereq": False},
+    ],
+)
+def test_get_cloud_ids_with_prereqs():
+    """Test get ids with prereqs."""
+
+    ids = get_cloud_ids_with_prereqs()
+
+    assert ids is not None
+    assert "aws" in ids
+    assert "azure" not in ids
+
+
+@patch(
+    "watchmaker.config.status.SUPPORTED_CLOUD_PROVIDERS",
+    [
+        {"provider": "aws", "has_prereq": True},
+        {"provider": "azure", "has_prereq": False},
+    ],
+)
+def test_get_cloud_ids_missing_prereqs():
+    """Test get ids missing prereqs."""
+
+    ids = get_cloud_ids_missing_prereqs()
+
+    assert ids is not None
+    assert "aws" not in ids
+    assert "azure" in ids
+
+
+@patch(
+    "watchmaker.config.status.SUPPORTED_NON_CLOUD_PROVIDERS",
+    [
+        {"provider": "file"},
+        {"provider": "db"},
+    ],
+)
+def test_get_non_cloud_identifiers():
+    """Test get non cloud identifiers matching config."""
+
+    config_status = {
+        "providers": [
+            {
+                "key": "WatchmakerStatus",
+                "required": False,
+                "provider_type": "sqs",
+            },
+            {
+                "key": "WatchmakerStatus",
+                "required": False,
+                "provider_type": "file",
+            },
+        ]
+    }
+
+    ids = get_non_cloud_identifiers(config_status)
+
+    assert ids is not None
+    assert "sqs" not in ids
+    assert "file" in ids
+
+
+@patch(
+    "watchmaker.config.status.SUPPORTED_NON_CLOUD_PROVIDERS",
+    [],
+)
+def test_get_non_cloud_identifiers():
+    """Test empty SUPPORTED_NON_CLOUD_PROVIDERS."""
+
+    config_status = {
+        "providers": [
+            {
+                "key": "WatchmakerStatus",
+                "required": False,
+                "provider_type": "sqs",
+            },
+            {
+                "key": "WatchmakerStatus",
+                "required": False,
+                "provider_type": "file",
+            },
+        ]
+    }
+
+    ids = get_non_cloud_identifiers(config_status)
+
+    assert ids == []
