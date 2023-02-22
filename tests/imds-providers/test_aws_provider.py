cd7a25fc0c74ef3e09bc53c5bd5e389e606ce76e
Dennis Carey <dwc0011@users.noreply.github.com>
2023-02-22 18:04:32 -0500

    Update tests to return bytes

--- tests/imds-providers/test_aws_provider.py
+++ tests/imds-providers/test_aws_provider.py
@@ -29,74 +29,86 @@ from watchmaker.utils.imds.detect.providers.aws_provider import AWSProvider
 #     assert provider.check_metadata_server() is False
 
 
-@patch.object(AWSProvider,
-              '_AWSProvider__get_data_from_server',
-              return_value=('{"imageId": "ami-12312412", \
-                            "instanceId": "i-ec12as"}'.encode("utf8")))
+@patch.object(
+    AWSProvider,
+    "_AWSProvider__get_data_from_server",
+    return_value=(
+        '{"imageId": "ami-12312412", \
+                            "instanceId": "i-ec12as"}'.encode(
+            "utf8"
+        )
+    ),
+)
 def test_aws_valid_metadata_data_from_server(provider_mock):
     """Test valid server data response for aws provider identification."""
     provider = AWSProvider()
     assert provider.check_metadata_server() is True
 
 
-@patch.object(AWSProvider,
-              '_AWSProvider__get_data_from_server',
-              return_value=('{"imageId": "some_ID", \
-                              "instanceId": "some_Instance"}'.encode("utf8")))
+@patch.object(
+    AWSProvider,
+    "_AWSProvider__get_data_from_server",
+    return_value=(
+        '{"imageId": "some_ID", \
+                              "instanceId": "some_Instance"}'.encode(
+            "utf8"
+        )
+    ),
+)
 def test_aws_invalid_metadata_data_from_server(provider_mock):
     """Test invalid server data response for aws provider identification."""
     provider = AWSProvider()
     assert provider.check_metadata_server() is False
 
 
-@patch.object(AWSProvider,
-              '_AWSProvider__get_data_from_server',
-              return_value=(None))
+@patch.object(
+    AWSProvider, "_AWSProvider__get_data_from_server", return_value=(None)
+)
 def test_aws_no_metadata_data_from_server(provider_mock):
     """Test no server data response for aws provider identification."""
     provider = AWSProvider()
     assert provider.check_metadata_server() is False
 
 
-@patch.object(AWSProvider,
-              '_AWSProvider__is_valid_server',
-              return_value=True)
+@patch.object(AWSProvider, "_AWSProvider__is_valid_server", return_value=True)
 def test_aws_metadata_valid_server(provider_mock):
     """Test valid server for aws provider identification."""
     provider = AWSProvider()
     assert provider.check_metadata_server() is True
 
 
-@patch.object(AWSProvider,
-              '_AWSProvider__is_valid_server',
-              return_value=False)
+@patch.object(AWSProvider, "_AWSProvider__is_valid_server", return_value=False)
 def test_aws_metadata_invalid_server(provider_mock):
     """Test invalid server for aws provider identification."""
     provider = AWSProvider()
     assert provider.check_metadata_server() is False
 
 
-@patch.object(AWSProvider,
-              '_AWSProvider__get_file_contents',
-              return_value="provider is amazon aws")
+@patch.object(
+    AWSProvider,
+    "_AWSProvider__get_file_contents",
+    return_value=b"provider is amazon aws",
+)
 def test_aws_valid_vendor_file(provider_mock):
     """Tests valid vendor file."""
     provider = AWSProvider()
     assert provider.check_vendor_file() is True
 
 
-@patch.object(AWSProvider,
-              '_AWSProvider__get_file_contents',
-              return_value="provider unknown")
+@patch.object(
+    AWSProvider,
+    "_AWSProvider__get_file_contents",
+    return_value=b"provider unknown",
+)
 def test_aws_invalid_vendor_file(provider_mock):
     """Tests invalid vendor file."""
     provider = AWSProvider()
     assert provider.check_vendor_file() is False
 
 
-@patch.object(AWSProvider,
-              '_AWSProvider__get_file_contents',
-              return_value=None)
+@patch.object(
+    AWSProvider, "_AWSProvider__get_file_contents", return_value=None
+)
 def test_aws_no_response_vendor_file(provider_mock):
     """Tests no response while reading vendor file."""
     provider = AWSProvider()
