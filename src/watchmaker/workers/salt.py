ae339f709d192be058323774e464c5e7b3de70bb
Ehinome Ihenyen <eihenyen@macbook-pro.myfiosgateway.com>
2023-03-02 14:28:31 -0500

    Resolving lint checks pt3

--- src/watchmaker/workers/salt.py
+++ src/watchmaker/workers/salt.py
@@ -780,6 +780,8 @@ class SaltLinux(SaltBase, LinuxPlatformManager):
         if self.salt_version and self.salt_version in current_salt_version:
             self.log.info('Salt is already installed.')
             return True
+        else:
+            return False
 
     def _install_package(self):
         if self.install_method.lower() == 'yum':
