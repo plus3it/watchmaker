3b31630e8f166980fc8b8ca422535ca29524aaf4
Thomas H Jones II <github@xanthia.com>
2025-09-17 14:09:40 -0400

    Script for prepared Docker image to run
    The Docker file prepares a Docker container for use, the script allows a
    "hands-free" executiong of the documentation-build by being invoked as
    part of a `run` operation.

---
+++ ci/local/build_dox.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -euo pipefail
+#
+# A simple script to make executing HTML-generation easy
+#
+###########################################################################
+
+# Navigate into the project content-directory
+cd /watchmaker || ( echo "Dir-change failed. Aborting..." ; exit 1 )
+
+# Build watchmaker
+python3 -m build
+
+# Install the just-built watchmaker python modules
+python3 -m pip install ./dist/watchmaker-*.whl
+
+# Build the HTML files
+sphinx-build -a -E -W --keep-going -b html docs dist/docs
+
+# Test the documentation
+sphinx-build -b doctest docs dist/docs
+
+# Test documentation's link-refs
+sphinx-build -b linkcheck docs dist/docs
+
